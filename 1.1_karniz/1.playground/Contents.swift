c22d4c6895e21c4db9acc74ed01faab3edbba1ea
kate <karniz@64mail.ru>
2025-10-15 11:19:45 -0700

    1.1karniz

---
+++ 1.1_karniz/1.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+//переменные описывающие профиль пользователя
+let name = "kate"
+let firstName = "karniz"
+var age = 24
+let gender = "woman"
+var aboutMe = "learning swift"
+let publicProfile = true
+
+//вывод переменных на консоль
+print("имя\(name)")
+print("фамилия\(firstName)")
+print("возраст\(age)")
+print("пол\(gender)")
+print("о себе\(aboutMe)")
+print("публичностьПрофиля\(publicProfile)")
