8b1dfd9fb9af85aee82fef9b830dbdc06b7559be
kate <karniz@64mail.ru>
2025-10-23 13:52:40 -0700

    1.4.1_karniz

---
+++ 1.4.1_karniz/1.4.1_karniz/1.4.1_karniz.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+let name = "Екатерина"
+let lastName = "Карниз"
+let age = "24"
+let hobby = "слушать музыку"
+let zp = "111111"
+
+var resume = """
+Имя: \(name)
+Фамилия: \(lastName)
+Возраст: \(age)
+Хобби: \(hobby)
+Зарплатные ожидания: \(zp)
+"""
+print (resume)
